a1ae2135ed6a6cae0df03299139f909c7cab9461
sonjayilmaz <89841270+sonjayilmaz@users.noreply.github.com>
2025-07-10 16:00:18 +0200

    Revert "Created core app for landing page and base layout. merged into dev"

--- dj_skillswap/settings.py
+++ dj_skillswap/settings.py
@@ -10,14 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dj_skillswap_app'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +54,7 @@ ROOT_URLCONF = 'dj_skillswap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR, STATIC_DIR, MEDIA_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,14 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Media directory
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = MEDIA_DIR
--- dj_skillswap_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from dj_skillswap_app.models import Skill
-from .models import Profile
-
-admin.site.register(Skill)
-admin.site.register(Profile)
