08902e891bfc158097cc79ef8aa69079846f6afb
miliyardd <mili.2000t@gmail.com>
2025-07-10 13:04:31 +0200

    Add user profile model and migrations

---
+++ dj_skillswap_app/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class DjSkillswapAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dj_skillswap_app'
+
+def ready(self):
+        import dj_skillswap_app.signals
---
+++ dj_skillswap_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} | {self.user.username}"
