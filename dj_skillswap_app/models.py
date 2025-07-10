e7ca3d1cb6b9b88c0fc4a47455ad9a2c5a856760
Dino C. <dino@adenogame.com>
2025-07-10 10:49:55 +0200

    feat: created app and skill model

---
+++ dj_skillswap_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Skill: {self.name} in the category :{category}"
