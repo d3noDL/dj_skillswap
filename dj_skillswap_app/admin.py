c45fc6e6aeabeabd868c5084760dafcce0158d27
miliyardd <mili.2000t@gmail.com>
2025-07-10 12:03:30 +0200

    Added  user profile model and admin integration

--- dj_skillswap_app/admin.py
+++ dj_skillswap_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from dj_skillswap_app.models import Skill
+from .models import Profile
 
 admin.site.register(Skill)
+admin.site.register(Profile)
--- dj_skillswap_app/models.py
+++ dj_skillswap_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class Skill(models.Model):
     name = models.CharField(max_length=255)
@@ -6,3 +7,13 @@ class Skill(models.Model):
 
     def __str__(self):
         return f"Skill: {self.name} in the category :{category}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} | {self.user.username}"
