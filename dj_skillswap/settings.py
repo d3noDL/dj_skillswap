e97febcb4a3fcac3ffedd2678ff1498224ff60f5
sonjayilmaz <sonja_yilmaz@hotmail.com>
2025-07-10 15:48:11 +0200

    Revert "Merge pull request #3 from d3noDL/landing-page-nav"
    This reverts commit 8ed6996fff5672a19c22dc4420cfacdded1fe56b, reversing
    changes made to 74a725928112eb376dce351fe266a72caf50286a.

--- dj_skillswap/settings.py
+++ dj_skillswap/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
 ]
 
 MIDDLEWARE = [
@@ -115,8 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'core/static']
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
