79b120a2d463f5dbc033e96ec1fee645deb02326
Dino C. <dino@adenogame.com>
2025-07-10 12:46:12 +0200

    chore: added comments, fixed str in models

--- dj_skillswap_app/models.py
+++ dj_skillswap_app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 
 class Skill(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"Skill: {self.name} in the category :{self.category}"
+        return f"Skill: {self.name} in the category: {self.category}"
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- dj_skillswap_app/views.py
+++ dj_skillswap_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from dj_skillswap_app.forms import AddSkillForm
 from dj_skillswap_app.models import Skill
+from django.contrib.auth.decorators import login_required
 
+#uncomment when we add authentication
+#@login_required
 def add_skill(request):
     skills = Skill.objects.all()
     if request.method == "POST":
