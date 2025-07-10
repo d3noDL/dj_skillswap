5ebe3ac944341d28056fb3dccdc73e9283971808
Dino C. <dino@adenogame.com>
2025-07-10 12:34:38 +0200

    chore: fixed models

--- dj_skillswap_app/models.py
+++ dj_skillswap_app/models.py
@@ -6,7 +6,7 @@ class Skill(models.Model):
     category = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"Skill: {self.name} in the category :{category}"
+        return f"Skill: {self.name} in the category :{self.category}"
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
