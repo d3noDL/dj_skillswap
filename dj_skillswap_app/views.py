eea332c17958cb590853aa5bc7470adc171c63da
dendenmuniz <dendenmuniz@gmail.com>
2025-07-11 13:32:20 +0200

    feat: enhance list_posts and add post_detail view for better data handling

--- dj_skillswap_app/views.py
+++ dj_skillswap_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from dj_skillswap_app.models import Skill, ProfileSkill
 from dj_skillswap_app.forms import AddProfileSkillForm
 from django.db.models import Q
@@ -39,7 +39,8 @@ def list_posts(request):
     if 'param' in request.GET:
         param = request.GET['param']
         query = Q(Q(skill__name__icontains=param) |
-                  Q(skill__category__icontains=param))
+                  Q(skill__category__icontains=param) |
+                  Q(type__icontains=param))
         posts = ProfileSkill.objects.filter(query)
     else:
         posts = ProfileSkill.objects.all()
@@ -50,13 +51,35 @@ def list_posts(request):
     ]
     rows = []
     for post in posts:
-        row = []
+        row = {
+            'id': post.id,  # Mantém o ID separado (para links)
+            'values': []
+        }
         for field in fields:
             # value = field.value_from_object(post)
             value = getattr(post, field.name)
             if field.is_relation and hasattr(value, 'pk'):
                 value = str(value)
-            row.append(value)
+            row['values'].append(value)
         rows.append(row)
     return render(request, 'dj_skillswap_app/browse_posts.html', context={'rows': rows,
                                                                           'fields': fields, })
+
+
+def post_detail(request, id):
+    post = get_object_or_404(ProfileSkill, pk=id)
+    fields = [field for field in ProfileSkill._meta.fields if field.name not in [
+        'id', 'created_at', 'updated_at']]
+
+    post_data = {}
+    for field in fields:
+        value = getattr(post, field.name)
+        value = str(value)
+        post_data[field.verbose_name.title()] = value
+        if post.type == 'Offer':
+            post_type = "I'm offering "
+            post_what = "What am I offering?"
+        else:
+            post_type = "I'm requesting "
+            post_what = "What am I resquesting?"
+    return render(request, 'dj_skillswap_app/post_details.html', context={'post_data': post, "post_type": post_type, "post_what": post_what})
