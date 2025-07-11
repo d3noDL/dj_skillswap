bcf4a9561e31fdc2fc83f328801082963bb2dabc
miliyardd <mili.2000t@gmail.com>
2025-07-11 12:57:02 +0200

    Fix login namespace error and apply styling updates

--- dj_skillswap/urls.py
+++ dj_skillswap/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from dj_skillswap_app import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/', views.view_profile, name='view_profile'),  
     path('accounts/', include('django.contrib.auth.urls')),
+    path('app/', include('dj_skillswap_app.urls', namespace='dj_skillswap_app')),
 
 ]
