a9d1cf0ce4d8e9a91f7c48be39f0a341d7109475
Denise Muniz <57671852+dendenmuniz@users.noreply.github.com>
2025-07-14 22:37:32 +0200

    Revert "Feat/browse skills"

--- dj_skillswap/urls.py
+++ dj_skillswap/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from dj_skillswap_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('app/', include('dj_skillswap_app.urls', namespace='dj_skillswap_app')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
--- dj_skillswap_app/apps.py
+++ dj_skillswap_app/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class DjSkillswapAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dj_skillswap_app'
-
-
-def ready(self):
-        import dj_skillswap_app.signals
-
