c520e571433bdd0449141ec8aecc9b45e68b5b07
dendenmuniz <dendenmuniz@gmail.com>
2025-07-17 15:58:39 +0200

    fix:  latest posts and total count to home page view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from dj_skillswap_app.models import Category
+from dj_skillswap_app.models import Category, UserProfileSkill
 
 
 def home(request):
     categories = Category.objects.all()
-    return render(request, 'core/home.html', {'categories': categories})
+    posts = UserProfileSkill.objects.all().order_by('-created_at')[:4]  # Get the latest 4 skills
+    total_posts = UserProfileSkill.objects.count()  # Get the total count of skills
+    return render(request, 'core/home.html', {'categories': categories, 'posts': posts, 'total_posts': total_posts})
