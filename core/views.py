273fa90ca5b26ff5eb1e4bc8245e93d104297d4e
dendenmuniz <dendenmuniz@gmail.com>
2025-07-17 15:53:45 +0200

    feat: update home view to include latest posts and total count in template

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from dj_skillswap_app.models import Category
+from dj_skillswap_app.models import Category, UserProfileSkill
 
 
 def home(request):
     categories = Category.objects.all()
-    return render(request, 'core/home.html', {'categories': categories})
+    posts = UserProfileSkill.objects.all().order_by('-created_at')[:4]  # Get the latest 5 skills
+    total_posts = UserProfileSkill.objects.count()
+    return render(request, 'core/home.html', {'categories': categories, 'posts': posts, 'total_posts': total_posts})
